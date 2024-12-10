5574c59915541d81fba07045997172f34d81cc54
tharunj30 <tharunkumar200230@gmail.com>
2024-12-10 21:44:03 +0530

    basic project template with get api

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+
+app.use(express.json())
+// routes
+app.get('/hello',(req,res)=>{
+    res.send('task manager app')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+// app.get('/api/v1/tasks) - get all the tasks
+// app.post('/api/v1/tasks) - create a new task
+// app.get('/api/v1/tasks/:id') - get single task
+// app.patch('/api/v1/tasks/:id) - update task
+// app.delete('/api/v1/tasks/:id) - delete task
+
+
+const port = 3000
+
+app.listen(port, console.log(`server listnening on ${port}`)
+)
