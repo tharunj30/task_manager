2e1ae8533052bd534ec6e413d1f8b3f2d85896b8
tharunj30 <tharunkumar200230@gmail.com>
2024-12-12 08:33:09 +0530

    set up connection of backend and db

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,18 +1,20 @@
 // REST Api - representation state transfer
+const Task = require('../models/task')
 
-const getAllTask = (req,res) =>{
+const getAllTask = (req, res) => {
     res.send('getting all tasks')
 }
-const createTask = (req,res) =>{
-    res.json(req.body)
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
 }
-const getTask = (req,res) =>{
-    res.json({ id: req.params.id})
+const getTask = (req, res) => {
+    res.json({ id: req.params.id })
 }
-const updateTask = (req,res) =>{
+const updateTask = (req, res) => {
     res.send('update task')
 }
-const deleteTask = (req,res) =>{
+const deleteTask = (req, res) => {
     res.send('delete task')
 }
 module.exports = {
