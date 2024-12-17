34e78eca9d6ce2e1e12d190ada02dfdb80287b4e
tharunj30 <tharunkumar200230@gmail.com>
2024-12-17 07:34:20 +0530

    gettask and create task api call updation

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,15 +1,34 @@
 // REST Api - representation state transfer
 const Task = require('../models/task')
 
-const getAllTask = (req, res) => {
-    res.send('getting all tasks')
+const getAllTask = async (req, res) => {
+    try {
+       const tasks = await Task.find({}) 
+       res.status(200).json({tasks})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
+    try {
+        const task = await Task.create(req.body)
     res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+    
 }
-const getTask = (req, res) => {
-    res.json({ id: req.params.id })
+const getTask = async (req, res) => {
+    try {
+        const{id:taskID} = req.params
+        const singleTask = await Task.findOne({_id:taskID})
+        if(!singleTask){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).json(singleTask)
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 const updateTask = (req, res) => {
     res.send('update task')
